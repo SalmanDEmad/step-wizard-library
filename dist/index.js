ec6254c2eb472d46df36d5911607ebbd6c1ece08
Salman Emad <salmanemad2003@gmail.com>
2024-09-13 01:25:36 +0300

    first upload thx

---
+++ dist/index.js
@@ -0,0 +1,13 @@
+"use strict";
+// src/index.ts
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.WizardSteps = exports.StepWizard = void 0;
+// Exporting components from the library
+var StepWizard_1 = require("./StepWizard");
+Object.defineProperty(exports, "StepWizard", { enumerable: true, get: function () { return __importDefault(StepWizard_1).default; } });
+var WizardSteps_1 = require("./WizardSteps");
+Object.defineProperty(exports, "WizardSteps", { enumerable: true, get: function () { return __importDefault(WizardSteps_1).default; } });
+//# sourceMappingURL=index.js.map
